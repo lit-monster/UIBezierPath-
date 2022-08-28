75522b8aefaa184c66aed98758e18a89fc6b0551
MasudaHinata <hinatamasuda0121@gmail.com>
2022-08-28 21:06:51 +0900

    UIBezierpath

--- UIBezierPath/ViewController.swift
+++ UIBezierPath/ViewController.swift
@@ -23,24 +23,26 @@ class ViewController: UIViewController {
 class DrawView: UIView {
     override func draw(_ rect: CGRect) {
         
-        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 0 ..< Float(rect.width))), y: CGFloat(Float.random(in: 0 ..< Float(rect.height)))))
-        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 0 ..< Float(rect.width))), y: CGFloat(Float.random(in: 0 ..< Float(rect.height)))))
-        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 0 ..< Float(rect.width))), y: CGFloat(Float.random(in: 0 ..< Float(rect.height)))))
-        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 0 ..< Float(rect.width))), y: CGFloat(Float.random(in: 0 ..< Float(rect.height)))))
-        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 0 ..< Float(rect.width))), y: CGFloat(Float.random(in: 0 ..< Float(rect.height)))))
-        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 0 ..< Float(rect.width))), y: CGFloat(Float.random(in: 0 ..< Float(rect.height)))))
-        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 0 ..< Float(rect.width))), y: CGFloat(Float.random(in: 0 ..< Float(rect.height)))))
+        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 100 ..< Float(rect.width))), y: CGFloat(Float.random(in: 60 ..< Float(rect.height * 0.75)))))
+        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 100 ..< Float(rect.width))), y: CGFloat(Float.random(in: 60 ..< Float(rect.height * 0.75)))))
+        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 100 ..< Float(rect.width))), y: CGFloat(Float.random(in: 60 ..< Float(rect.height * 0.75)))))
+        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 100 ..< Float(rect.width))), y: CGFloat(Float.random(in: 60 ..< Float(rect.height * 0.75)))))
+        graph(vertex: CGPoint(x: CGFloat(Float.random(in: 100 ..< Float(rect.width))), y: CGFloat(Float.random(in: 60 ..< Float(rect.height * 0.75)))))
         
     }
     func graph(vertex: CGPoint) {
-        let point1 = CGPoint(x: 0, y: 0)
-        let point2 = CGPoint(x: 0, y: 150)
-        let point3 = CGPoint(x: vertex.x, y: vertex.y - 100)
-        let point4 = vertex
-        let point5 = CGPoint(x: vertex.x, y: vertex.y + 100)
-        let point6 = CGPoint(x: 0, y: self.bounds.height - 150)
-        let point7 = CGPoint(x: 0, y: self.bounds.height)
+        let delta = min(bounds.height - vertex.y, vertex.y) * 0.55
+        let deltaTop = vertex.y - delta
+        let deltaBottom = self.bounds.height - vertex.y - delta
         
+        let point1 = CGPoint(x: 0, y: 0)
+        let point2 = CGPoint(x: 0, y: deltaTop)
+        let point3 = CGPoint(x: vertex.x, y: vertex.y - delta)
+        let point4 = vertex
+        let point5 = CGPoint(x: vertex.x, y: vertex.y + delta)
+        let point6 = CGPoint(x: 0, y: self.bounds.height - deltaBottom)
+        let point7 = CGPoint(x: 0, y: self.bounds.height)
+    
         let path = UIBezierPath()
         path.move(to: point1)
         path.addCurve(to: point4, controlPoint1: point2, controlPoint2: point3)
